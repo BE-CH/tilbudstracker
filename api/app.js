b35b2b6207e16f1b61f57d6e61937887610e1213
BE-CH <mikkel.l.bech@gmail.com>
2021-06-12 19:02:01 +0200

    + updated api

--- api/app.js
+++ api/app.js
@@ -1,15 +1,27 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const helmet = require('helmet');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+app.use(helmet());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(logger('dev'));
+}
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(compression());
+}
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
