5409c52a6fd5a95c4c0b5edcce010070ae8e2263
BE-CH <mikkel.l.bech@gmail.com>
2022-08-12 00:14:32 +0200

    + renamed and added scrollview

--- app/ios/TilbudstrackerTests/TilbudstrackerTests.m
+++ app/ios/TilbudstrackerTests/TilbudstrackerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface tilbudstracker_appTests : XCTestCase
+@interface TilbudstrackerTests : XCTestCase
 
 @end
 
-@implementation tilbudstracker_appTests
+@implementation TilbudstrackerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
